f02e502b7517d71ff91f7bcf0f6ec3dedf5a3f1c
Victor Santos <victorluizsantos49@gmail.com>
2022-10-15 13:31:29 -0300

    Cadastro base concluido
    * Há coisas a melhorar
    
    * Modificado:
    
    - Register.cs
    - User.cs
    
    * Criado:
    - [x] base do cadastro, precisa ser padrão exato
    - [x] método principal para chamada
    - [x] método para visualizar todas prop do user
    
    * Alterado:
    - prop PhoneDDD apagada, prop CodeArea é a correta
    - Valor para prop CodeArea atribuido
    - Class User adicionada herança com Adress
    - Prop Adress na class User apagada devido a nova herança

--- EasyBank/Register.cs
+++ EasyBank/Register.cs
@@ -7,21 +7,26 @@ namespace EasyBank
     {
         public void UserRegister(User user)
         {
-            //R_Name(user);
-            //R_Age_DateBorn(user);
-            //R_Adress(user);
-            //R_CPF(user);
-            //R_RG(user);
+            R_Name(user);
+            R_Age_DateBorn(user);
             R_PhoneNumber(user);
+            R_Email(user);
+            R_Password(user);
+            R_CPF(user);
+            R_RG(user);
+            R_Adress(user);
+            R_MonthlyIncome(user);
+            R_SafetyKey(user);
+            R_CreditCard(user);
         }
         public void R_Name(User user)
         {
-            Console.Write("Digite seu nome: ");
+            Console.Write("Cadastre seu nome: ");
             user.Name = Console.ReadLine();
         }
         public void R_Age_DateBorn(User user) // improve
         {
-            Console.Write("Digite sua data de nascimento no formato 00/00/0000\nDigite: ");
+            Console.Write("Cadastre sua data de nascimento no formato 00/00/0000\nDigite: ");
             string userInputDateBorn = Console.ReadLine();
             user.DateBorn = DateTime.ParseExact(userInputDateBorn, "dd/MM/yyyy", null);
 
@@ -34,6 +39,7 @@ namespace EasyBank
             Adress adress = new Adress();
             user.Country = adress.Country;
 
+            Console.WriteLine("Cadastre seus dados de endereço");
             Console.Write("Cidade: ");
             user.City = Console.ReadLine();
             Console.Write("Estado: ");
@@ -47,37 +53,89 @@ namespace EasyBank
             Console.Write("Complemento: ");
             user.HouseComplement = Console.ReadLine();
 
-            user.FullAdress = $"País: {user.Country}\nCidade: {user.City}\nEstado: {user.State}\nBairro: " +
+            user.FullAdress = $"\nPaís: {user.Country}\nCidade: {user.City}\nEstado: {user.State}\nBairro: " +
                 $"{user.Neiborhood}\n" +
                 $"Rua: {user.Street}\nNumero: {user.HouseNumber}\nComplemento: {user.HouseComplement}\n";
         }
         public void R_CPF(User user)
         {
-            Console.Write("CPF: ");
+            Console.Write("Cadastre seu CPF: ");
             user.CPF = Console.ReadLine();
         }
         public void R_RG(User user)
         {
-            Console.Write("RG: ");
+            Console.Write("Cadastre seu RG: ");
             user.RG = Console.ReadLine();
         }
         public void R_PhoneNumber(User user)
         {
-            user.PhoneDDD = "+55";
-            Console.Write("Telefone: ");
-            var userInputPhone = Console.ReadLine();
-            if (userInputPhone.Length < 11)
+            var inProcess = true;
+            while (inProcess)
             {
-                Console.WriteLine("Telefone inválido, tente novamente");
-            }
-            else
-            {
-                user.PhoneNumber = user.PhoneDDD + userInputPhone;
+                Console.Write("Cadastre seu telefone: ");
+                var userInputPhone = Console.ReadLine();
+                if (userInputPhone.Length < 11)
+                {
+                    Console.WriteLine("Telefone inválido, tente novamente");
+                }
+                else
+                {
+                    user.PhoneNumber = user.PhoneCodeArea + userInputPhone;
+                    inProcess = false;
+                }
             }
         }
-        public void R_CreditCard()
+        public void R_CreditCard(User user) //Add limit
         {
-            //Implement
+            CreditCard creditCard = new CreditCard();
+            Random random = new Random();
+            creditCard.CVV = Convert.ToString(random.Next(101, 999));
+            creditCard.NameOwner = user.Name;
+            creditCard.ExpireDate = DateTime.Today.AddYears(3);
+        }
+        public void R_Email(User user)
+        {
+            Console.Write("Cadastre seu email: ");
+            user.Email = Console.ReadLine();
+        }
+        public void R_Password(User user)
+        {
+            Console.Write("Cadastre sua senha: ");
+            user.Password = Console.ReadLine();
+        }
+        public void R_MonthlyIncome(User user)
+        {
+            Console.WriteLine("Cadastre sua renda mensal bruta");
+            Console.Write("Digite: ");
+            user.MonthlyIncome = Convert.ToInt32(Console.ReadLine());
+        }
+        public void R_SafetyKey(User user)
+        {
+            var inProcess = true;
+            while (inProcess)
+            {
+                Console.WriteLine("Cadastre sua senha de segurança. Mínimo de 3 numeros");
+                Console.Write("Digite: ");
+                var userInputSafetyKey = Console.ReadLine();
+                if (userInputSafetyKey.Length < 3)
+                {
+                    Console.WriteLine("Máximo de 3 numeros, tente novamente");
+                }
+                else
+                {
+                    user.SafetyKey = userInputSafetyKey;
+                    inProcess = false;
+                }
+            }
+        }
+        public void ViewFullUserData(User user)
+        {
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(user))
+            {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(user);
+                Console.WriteLine("{0}={1}", name, value);
+            }
         }
     }
 }
--- EasyBank/User.cs
+++ EasyBank/User.cs
@@ -13,14 +13,12 @@
         public string CPF { get; set; } //CPF
         public string RG { get; set; } //RG
         public string PhoneNumber { get; set; } //NumeroTelefone
-        public string PhoneCodeArea { get; set; } // Codigo de area
-        public string PhoneDDD { get; set; } //DDD telefone -- talvez apagar
-        public string Adress { get; set; } //Endereço
+        public string PhoneCodeArea { get; set; } = "+55"; // Codigo de area
         public string Password { get; set; } //Senha
         public string SafetyKey { get; set; } //SenhaSegurança (senha 3 digitos)
         public string Country { get; set; } = "Brasil"; //País
         public DateTime DateBorn { get; set; } //DataNascimento
         public bool AutoDebit { get; set; } //DébitoAutomatico
-        public int Age { get; set; } 
+        public int Age { get; set; } //Idade
     }
 }
