8c059adb61f0400d11ef241891a3aa02e97cdf92
Thiago Lacerda <lacerdathiagosilva.99@gmail.com>
2022-10-19 19:55:40 -0300

    primeira versão letras de segurança

--- EasyBank/SafetyPassword.cs
+++ EasyBank/SafetyPassword.cs
@@ -1,21 +1,20 @@
-﻿namespace EasyBank
+﻿using System.Runtime.ExceptionServices;
+
+namespace EasyBank
 {
     public class SafetyPassword
     {
 
-        //static string letraUM = "";
-        //static string letraDois = "";
-        //static string letraT8es = "";
+        static string SafetyKey { get; set; }
+        public string CriacaoTresLetras()
+        {
+            Console.WriteLine("Crie 3 letras de segurança");
+            SafetyKey = Console.ReadLine().ToUpper();
+            Console.Clear();
+            return SafetyKey;
+        }
 
-        //static void SelecionarLetrasChaves()
-        //{
-        //    letraUM = Console.ReadLine();
-        //    letraDois = Console.ReadLine();
-        //    letraT8es = Console.ReadLine();
-        //}
-
-
-        public void GerarLetras()
+        static void GerarLetras()
         {
             var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string[] charsarr = new string[6];
@@ -29,13 +28,43 @@
                     int posicaoLetra = random.Next(0, 25);
                     string letra = characters[posicaoLetra].ToString();
                     charsarr[i] = charsarr[i] + letra;
-
                 }
             }
-
             foreach (var item in charsarr)
             {
                 Console.WriteLine(item);
+            }
+        }
+
+        public void VerificarSenha()
+        {
+
+            for (int i = 3; i > -1;)
+            {
+                GerarLetras();
+
+                Console.WriteLine("Informe as suas letras de segurança");
+
+                var ConfirmarSenhaLetras = Console.ReadLine().ToUpper();
+                Console.WriteLine();
+
+
+                if (SafetyKey == ConfirmarSenhaLetras)
+                {
+                    Console.WriteLine("Senha correta");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine($"Senha incorreta, você possui {i--} chances ");
+
+                }
+                if (i == -1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Quantidade máxima de tentativas excedida! Você foi deslogado.");
+                    Thread.Sleep(2000);
+                }
             }
         }
     }
