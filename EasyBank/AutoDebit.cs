b4a878cc0b9023c6fc5aa86a9925afca9747ae62
Cailane Rodrigues Silveira <cailanee77@gmail.com>
2022-10-19 22:46:43 -0300

    base AutoDebit e opção 1 do menu

--- EasyBank/AutoDebit.cs
+++ EasyBank/AutoDebit.cs
@@ -4,5 +4,55 @@
     {
         public int Id { get; set; }
         public bool Activated { get; set; }
+        public void Menu()
+        {
+            bool ValidaMenu = true;
+
+            while (ValidaMenu)
+            {
+                Console.WriteLine("1 - O que é\n2 - Meus DB's\n3 - Cadastrar Nova Conta\n4 - Desativar \n5 - Sair");
+
+                Console.Write("Digite uma opção: ");
+                string optionMenu = Console.ReadLine();
+
+                if (optionMenu == "1")
+                {
+                    Explicacao();
+
+                }
+                else if (optionMenu == "2")
+                {
+                    Console.WriteLine("Visualizar Débitos Automáticos");
+                }
+                else if (optionMenu == "3")
+                {
+                    Console.WriteLine("Cadastrar novo Débito Automático");
+                }
+                else if (optionMenu == "4")
+                {
+                    Console.WriteLine("Desativar Débito Automático");
+                }
+                else if (optionMenu == "5")
+                {
+                    Console.WriteLine("Saindo...");
+                    ValidaMenu = false;
+                }
+                else
+                {
+                    Console.WriteLine("Digite uma opção válida (Opções: 1, 2, 3, 4 ou 5)");
+                    optionMenu = Console.ReadLine();
+                }
+            }
+        }
+
+        public void Explicacao()
+        {
+            Console.WriteLine("Está cada dia mais difícil lembrar da data de vencimento de suas contas?");
+            Console.WriteLine("Com o débito automático você programa suas contas para serem pagas de forma automática na data escolhida por você!");
+            Console.WriteLine("É simples: na opção '3' do menu anterior, você irá informar qual conta deseja pagar:");
+            Console.WriteLine("(Ex: água, luz) e todo mês, na data que você escolher, debitamos o valor da sua conta e pronto! Suas contas estão pagas, sem trabalho extra!");
+            Console.WriteLine("Você pode visualizar quais contas estão em débitos automáticos na opção '2'.");
+            Console.WriteLine("Quer remover uma conta do débito automático? Acesse a opção '4'.");
+        }
     }
 }
--- EasyBank/Program.cs
+++ EasyBank/Program.cs
@@ -1,30 +1,36 @@
-﻿using EasyBank;
+﻿//using EasyBank;
 
-var ProgramStarted = true;
-User user = new User();
+//var ProgramStarted = true;
+//User user = new User();
 
-while (ProgramStarted)
-{
-    Thread.Sleep(1000);
-    Console.Clear();
-    Console.WriteLine("1 - Registrar\n2 - Entrar\n3 - Fechar\n");
-    Console.Write("Digite: ");
-    var userInputMainMenu = Console.ReadLine();
+//while (ProgramStarted)
+//{
+//    //Thread.Sleep(1000);
+//    //Console.Clear();
+//    //Console.WriteLine("1 - Registrar\n2 - Entrar\n3 - Fechar\n");
+//    //Console.Write("Digite: ");
+//    //var userInputMainMenu = Console.ReadLine();
 
-    if (userInputMainMenu == "1")
-    {
+//    if (userInputMainMenu == "1")
+//    {
 
-    }
-    else if (userInputMainMenu == "2")
-    {
+//    }
+//    else if (userInputMainMenu == "2")
+//    {
 
-    }
-    else if (userInputMainMenu == "3")
-    {
+//    }
+//    else if (userInputMainMenu == "3")
+//    {
 
-    }
-    else
-    {
-        Console.WriteLine("opção não disponível, tente novamente");
-    }
-}
+//    }
+//    else
+//    {
+//        Console.WriteLine("opção não disponível, tente novamente");
+//    }
+//}
+
+using EasyBank;
+
+AutoDebit autoDebit = new AutoDebit();
+
+autoDebit.Menu();
