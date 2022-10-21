c4571bbdf2d02d979837168110723c4c4a1a1a1a
CAMILA HUNCKEL SCHIOCHET <hs.camila@gmail.com>
2022-10-20 21:45:48 -0300

    base LogIn

--- EasyBank/LogIn.cs
+++ EasyBank/LogIn.cs
@@ -2,5 +2,31 @@
 {
     public class LogIn
     {
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+        public string VerificarCPF()
+        {
+            Console.WriteLine("Digite seu CPF: ");
+            CPF = Console.ReadLine();
+            return CPF;
+        }
+        public string VerificarEmail()
+        {
+            Console.WriteLine("Digite seu Email: ");
+            Email = Console.ReadLine();
+            return Email;
+        }
+        public string VerificarPassaword()
+        {
+            Console.WriteLine("Digite sua senha: ");
+            Passaword = Console.ReadLine();
+            return Passaword;
+        }
+        public bool VerificadorLogin()
+        {
+
+        }
     }
 }
