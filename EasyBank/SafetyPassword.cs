ff08cccfcedfbb2db1053cc01a443e1c1c3548a0
THIAGO LACERDA DA SILVA <lacerdathiagosilva.99@gmail.com>
2022-10-21 21:30:42 -0300

    verificacao quantidade de letras adicionadas e orçanizacao do código

--- EasyBank/SafetyPassword.cs
+++ EasyBank/SafetyPassword.cs
@@ -2,31 +2,23 @@
 using System.Runtime.ExceptionServices;
 
 
-//Register register = new Register();
 //SafetyPassword pass = new SafetyPassword();
-//pass.CriacaoTresLetras();
-//pass.VerificarSenha();
+//pass.CriacaoTresLetras(user);
+//pass.VerificarLetras(user);
 namespace EasyBank
 {
     public class SafetyPassword
     {
+
         public string CriacaoTresLetras(User user)
         {
             string quantidadeLetras;
             Console.WriteLine("Crie 3 letras de segurança");
             quantidadeLetras = Validator.OutputNoNumberAndSpecialCaracteres(Console.ReadLine().ToUpper());
 
-            while (quantidadeLetras.Length < 3 || quantidadeLetras.Length > 3)
-            {
-                Console.WriteLine("Favor informar apenas 3 letras");
-                GerarLetras();
-                quantidadeLetras = Validator.OutputNoNumberAndSpecialCaracteres(Console.ReadLine());
-                Console.Clear();
+            quantidadeLetras = ConfirmarQuantLetras(quantidadeLetras);
 
-            }
-
-            Console.WriteLine($"Voce escolheu as Letras    '{quantidadeLetras}'  Digite sim para confirmar ou não para inserir novamente");
-            string confirmacao = Console.ReadLine().ToUpper();
+            string confirmacao = ConfirmacaoTresLetras(quantidadeLetras);
 
             if (confirmacao == "sim".ToUpper())
             {
@@ -35,46 +27,103 @@ namespace EasyBank
                 Console.WriteLine();
             }
 
-            while (confirmacao != "não".ToUpper() && confirmacao != "sim".ToUpper())
-            {
+            confirmacao = DiferenteSimOuNao(confirmacao);
 
-                Console.WriteLine("Favor escreva sim ou não corretamente");
-                confirmacao = Validator.OutputNoNumberAndSpecialCaracteres(Console.ReadLine().ToUpper());
-                Console.Clear();
-
-            }
-            if (confirmacao == "não".ToUpper())
+            while (confirmacao == "não".ToUpper())
             {
                 Console.WriteLine("Favor insira novamente as 3 letras");
-                    GerarLetras();
+                GerarLetras();
                 quantidadeLetras = Validator.OutputNoNumberAndSpecialCaracteres(Console.ReadLine().ToUpper());
-                Console.Clear();
-                while (quantidadeLetras.Length < 3 || quantidadeLetras.Length > 3)
-                {
-                    Console.WriteLine("Favor informar apenas 3 letras");
-                    Console.Clear();
+                quantidadeLetras = ConfirmarQuantLetras(quantidadeLetras);
+                confirmacao = ConfirmacaoTresLetras(quantidadeLetras);
+                confirmacao = DiferenteSimOuNao(confirmacao);
 
-                }
+                Console.Clear();
+
             }
 
             user.SafetyKey = quantidadeLetras;
             return quantidadeLetras;
         }
 
+        public static string ConfirmarQuantLetras(string confirmarQuantidade)
+        {
+            while (confirmarQuantidade.Length < 3 || confirmarQuantidade.Length > 3)
+            {
+                Console.WriteLine("Favor informar apenas 3 letras");
+                GerarLetras();
+                confirmarQuantidade = Validator.OutputNoNumberAndSpecialCaracteres(Console.ReadLine());
+                Console.Clear();
+
+            }
+            return confirmarQuantidade;
+        }
+
+        public static string ConfirmacaoTresLetras(string ConfigDasTresLetras)
+        {
+            Console.WriteLine($"Voce escolheu as Letras    '{ConfigDasTresLetras}'  Digite sim para confirmar ou não para inserir novamente");
+            ConfigDasTresLetras = Console.ReadLine().ToUpper();
+            return ConfigDasTresLetras;
+        }
+
+        public static string DiferenteSimOuNao(string VerificacaoConfirmacao)
+        {
+
+            while (VerificacaoConfirmacao != "não".ToUpper() && VerificacaoConfirmacao != "sim".ToUpper())
+            {
+
+                Console.WriteLine("Favor escreva sim ou não corretamente");
+                VerificacaoConfirmacao = Validator.OutputNoNumberAndSpecialCaracteres(Console.ReadLine().ToUpper());
+                Console.Clear();
+
+            }
+            return VerificacaoConfirmacao;
+        }
+
         static void GerarLetras()
         {
             var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string[] charsarr = new string[6];
+            var finalCharacters = characters;
+            string[] charsarr = new string[5];
 
             var random = new Random();
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    int posicaoLetra = random.Next(0, 25);
+                    int posicaoLetra = random.Next(0, characters.Length);
                     string letra = characters[posicaoLetra].ToString();
+
+                    if (charsarr[i] != null && charsarr[i].Contains(letra))
+                    {
+                        j--;
+
+                        continue;
+                    }
+
                     charsarr[i] = charsarr[i] + letra;
+                    characters = characters.Replace(letra, string.Empty);
+
+                    if (characters.Count() == 1)
+                    {
+                        for (int x = 0; x < charsarr.Length; x++)
+                        {
+                            if (x == 0)
+                            {
+                                letra = characters[0].ToString();
+
+                                charsarr[x] = charsarr[x] + letra;
+
+                                x++;
+                            }
+
+                            int posicaoLetraFinal = random.Next(0, finalCharacters.Length);
+                            string letraFinal = finalCharacters[posicaoLetraFinal].ToString();
+
+                            charsarr[x] = charsarr[x] + letraFinal;
+                        }
+                    }
                 }
             }
             foreach (var item in charsarr)
