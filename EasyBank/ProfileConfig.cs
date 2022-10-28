f50b46091959b2423309ffd1f3750eb755170abf
PedroValleMaff <pedrovalle1602@gmail.com>
2022-10-27 21:49:55 -0300

    inicio-profileConfig

--- EasyBank/ProfileConfig.cs
+++ EasyBank/ProfileConfig.cs
@@ -2,6 +2,32 @@
 {
     public class ProfileConfig
     {
+        public void ChangeRegistration(string nomeDoUsuario, string emaiDoUsuario, string telefoneDoUsuario, string dataDeNasimentoDoUsuario)
+        {
+            Console.Write("Qual dado será alterado?\n");
+            Console.Write($"\n1-Nome: {nomeDoUsuario}\n2-E-mail: {emaiDoUsuario}\n3-Telefone: {telefoneDoUsuario}\n4-Data de Nascimento: {dataDeNasimentoDoUsuario}");
+            Console.Write("\n\nInsira a opção\n->");
+            string opcao = Console.ReadLine();
 
+            switch (opcao)
+            {
+                case "1":
+
+                    break;
+
+                case "2":
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                case "4":
+
+                    break;
+
+            }
+        }
     }
 }
