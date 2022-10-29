0792bdb391ecb4fedbda75c4d92868c57cf98012
Cailane Rodrigues Silveira <cailanee77@gmail.com>
2022-10-29 17:27:06 -0300

    conflito resolvido

--- EasyBank/Program.cs
+++ EasyBank/Program.cs
@@ -1,68 +1,32 @@
-﻿////using EasyBank;
+﻿using EasyBank;
 
-//<<<<<<< HEAD
-////var ProgramStarted = true;
-////User user = new User();
-//=======
-//var ProgramStarted = true;
-//User user = new User();
-//List<User> listUser = new List<User>();
-//Register register = new Register();
-//>>>>>>> 60daccff2eaa7aba2aa5a4bc1680ba7d53f0f7c4
+User user = new User();
+var ProgramStarted = true;
+List<User> listUser = new List<User>();
+Register register = new Register();
+while (ProgramStarted)
+{
+    Thread.Sleep(1000);
+    Console.Clear();
+    Console.WriteLine("1 - Registrar\n2 - Entrar\n3 - Fechar\n");
+    Console.Write("Digite: ");
+    var userInputMainMenu = Console.ReadLine();
 
-////while (ProgramStarted)
-////{
-////    //Thread.Sleep(1000);
-////    //Console.Clear();
-////    //Console.WriteLine("1 - Registrar\n2 - Entrar\n3 - Fechar\n");
-////    //Console.Write("Digite: ");
-////    //var userInputMainMenu = Console.ReadLine();
+    if (userInputMainMenu == "1")
+    {
 
-//<<<<<<< HEAD
-////    if (userInputMainMenu == "1")
-////    {
+    }
+    else if (userInputMainMenu == "2")
+    {
 
-////    }
-////    else if (userInputMainMenu == "2")
-////    {
+    }
+    else if (userInputMainMenu == "3")
+    {
 
-////    }
-////    else if (userInputMainMenu == "3")
-////    {
+    }
+    else
+    {
+        Console.WriteLine("opção não disponível, tente novamente");
+    }
 
-////    }
-////    else
-////    {
-////        Console.WriteLine("opção não disponível, tente novamente");
-////    }
-////}
-
-//using EasyBank;
-
-//AutoDebit autoDebit = new AutoDebit();
-
-//autoDebit.Menu();
-//=======
-//    if (userInputMainMenu == "1")
-//    {
-//        register.UserRegister(user, listUser);
-//    }
-//    else if (userInputMainMenu == "2")
-//    {
-
-//    }
-//    else if (userInputMainMenu == "3")
-//    {
-//        ProgramStarted = false;
-//    }
-//    else
-//    {
-//        Console.WriteLine("opção não disponível, tente novamente");
-//    }
-//}
-//>>>>>>> 60daccff2eaa7aba2aa5a4bc1680ba7d53f0f7c4
-
-using EasyBank;
-
-AutoDebit autoDebit = new AutoDebit();
-autoDebit.Menu();
+}
