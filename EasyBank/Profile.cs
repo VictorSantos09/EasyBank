914419ce6eaf76a3f300439757e8f121e47da817
PEDRO VALLE MAFESSOLLI <pedrovalle1602@gmail.com>
2022-10-31 21:13:17 -0300

    safety-key-profile

--- EasyBank/Profile.cs
+++ EasyBank/Profile.cs
@@ -132,7 +132,7 @@
                     }
                     else
                     {
-                        AccountCancellation();
+                       AccountCancellation();
                     }
                 }
             }
