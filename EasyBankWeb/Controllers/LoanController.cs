a96242425027d46bb2c994ada730f197efe84a23
PEDRO VALLE MAFESSOLLI <pedro.mafessolli@alunos.sc.senac.br>
2022-11-25 20:00:08 -0300

    web-classe-sloancontroller

--- EasyBankWeb/Controllers/LoanController.cs
+++ EasyBankWeb/Controllers/LoanController.cs
@@ -1,6 +1,32 @@
-﻿namespace EasyBankWeb.Controllers
+﻿using EasyBankWeb.Entities;
+using EasyBankWeb.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBankWeb.Controllers
 {
-    public class LoanController
+    [ApiController]
+    [Route("[controller]")]
+    public class LoanController : ControllerBase
     {
+        private readonly LoanRepository _loanRepository;
+
+        public LoanController(LoanRepository laonRepository)
+        {
+            _loanRepository = laonRepository;
+        }
+
+        [HttpGet(Name = "GetLoans")]
+        public IActionResult Get()
+        {
+            return Ok(_loanRepository.GetLoan());
+        }
+
+        [HttpPost(Name = "PostLoan")]
+        public IActionResult Post([FromBody] Loan loan)
+        {
+            _loanRepository.AddLoan(loan);
+
+            return Ok();
+        }
     }
 }
