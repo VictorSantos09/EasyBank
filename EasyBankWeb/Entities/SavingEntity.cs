4b72a3e36bca798fd8b337d8f02e005aa9e6d619
VICTOR LUIZ SILVA SANTOS <victorluizsantos49@gmail.com>
2022-11-25 21:52:13 -0300

    saving entity added

---
+++ EasyBankWeb/Entities/SavingEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyBankWeb.Entities
+{
+    public class SavingEntity : BaseEntity
+    {
+        public double Value { get; set; }
+        public double TaxesValue { get; set; }
+        public double StartValue { get; set; }
+        public int MonthsPassed { get; set; } = 1;
+        public int OwnerID { get; set; }
+    }
+}
