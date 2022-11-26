7a0fe2a06c7f207d83067cd87e82630478e15411
VICTOR LUIZ SILVA SANTOS <victorluizsantos49@gmail.com>
2022-11-25 21:06:52 -0300

    teste2

---
+++ EasyBankWeb/Dto/SavingsDto.cs
@@ -0,0 +1,12 @@
+﻿using EasyBankWeb.Entities;
+
+namespace EasyBankWeb.Dto
+{
+    public class SavingsDto : BaseEntity
+    {
+        public double Value { get; set; }
+        public double TaxesValue { get; set; }
+        public double StartValue { get; set; }
+        public int MonthsPassed { get; set; } = 1;
+    }
+}
